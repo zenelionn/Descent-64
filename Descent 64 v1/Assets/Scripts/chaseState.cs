c4a46ad50169292f7ca2e70a602f58d5f7e2f018
zenelionn <zenelionn@gmail.com>
2025-01-31 14:51:59 +0000

    enemy now patrols and will run after player if too close

--- Descent 64 v1/Assets/Scripts/chaseState.cs
+++ Descent 64 v1/Assets/Scripts/chaseState.cs
@@ -1,40 +1,33 @@
 using System.Collections;
+using UnityEngine.AI;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class idleState : StateMachineBehaviour
+public class chaseState : StateMachineBehaviour
 {
-
-    float timer;
+    NavMeshAgent agent;
     Transform player;
-    float chaseRange = 8;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       timer = 0;
-       player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = animator.GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent.speed = 3.5f;
+
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        timer += Time.deltaTime;
-        if(timer > 5){
-            animator.SetBool("isPatrolling", true);
-        }
-
-        float distance = Vector3.Distance(player.position, animator.transform.position);
-        if (distance < chaseRange){
-            animator.SetBool("isChasing", true);
-        }
+        agent.SetDestination(player.position);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        agent.SetDestination(animator.transform.position);
     }
-    
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
--- Descent 64 v1/Assets/Scripts/patrolState.cs
+++ Descent 64 v1/Assets/Scripts/patrolState.cs
@@ -9,10 +9,15 @@ public class patrolState : StateMachineBehaviour
     List<Transform> wayPoints = new List<Transform>();
     NavMeshAgent agent;
 
+    Transform player;
+    float chaseRange = 8;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         agent = animator.GetComponent<NavMeshAgent>();
+        agent.speed = 1.5f;
         timer = 0;
         GameObject go = GameObject.FindGameObjectWithTag("WayPoints");
         foreach (Transform t in go.transform){
@@ -31,6 +36,11 @@ public class patrolState : StateMachineBehaviour
         timer += Time.deltaTime;
         if(timer > 10){
             animator.SetBool("isPatrolling", false);
+        }
+
+        float distance = Vector3.Distance(player.position, animator.transform.position);
+        if (distance < chaseRange){
+            animator.SetBool("isChasing", true);
         }
     }
 
