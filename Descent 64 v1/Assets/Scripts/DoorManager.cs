0a27fbbb9f0876d795aedb36beae708f9abd5cda
Eli <148476551+zenelionn@users.noreply.github.com>
2025-02-24 15:39:42 +0000

    crhist finally the washroom is locked haha

--- Descent 64 v1/Assets/Scripts/DoorManager.cs
+++ Descent 64 v1/Assets/Scripts/DoorManager.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class DoorManager : MonoBehaviour
 {
+    [Header("Door Stuff")]
     [SerializeField] private GameObject door;
     [SerializeField] private Animator doorAnimator;
     [SerializeField] private KeyCode interactionKey = KeyCode.E;
+    [SerializeField] private bool washroom;
     private bool canInteract;
     private bool doorOpen;
     private Collider playerCollider;
 
+    public static bool isLocked = true;
+ 
+    [Header("Canvas Stuff")]
+    [SerializeField] private Canvas dialogueCanvas;
+    [SerializeField] private TMP_Text dialogueText;
+  
     private void Start(){
         canInteract = false;
+        dialogueCanvas.enabled = false;
+
         //doorAnimator.SetBool("isOpen", false);
     }
 
     private void OnTriggerEnter(Collider other){
         if (other.GetComponent<Collider>() != null){
-            playerCollider = other;
-            canInteract = true;
-            doorOpen = false;
+                playerCollider = other;
+                canInteract = true;
+                doorOpen = false;
+            
         }
     }
 
@@ -28,6 +39,8 @@ public class DoorManager : MonoBehaviour
         if(other == playerCollider){
             canInteract = false;
             CloseDoor();
+            dialogueCanvas.enabled = false;
+            
         }
         
     }
@@ -35,7 +48,17 @@ public class DoorManager : MonoBehaviour
     private void Update(){
         if (canInteract && Input.GetKeyDown(interactionKey)){
             {
-                OpenDoor();
+                // if it's the washroom, check if it's locked. If not open door
+                if (washroom == true && isLocked == true){
+                    dialogueText.SetText("It's locked");
+                    dialogueCanvas.enabled = true;
+
+                }
+                
+                else{
+                    OpenDoor();
+                }
+                
             }
         }
     }
