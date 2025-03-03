e05126e1be19fac07913dc4660403a3e3a0c1500
Eli <148476551+zenelionn@users.noreply.github.com>
2025-03-03 17:11:34 +0000

    finished cutscene three
    its a little broken. some animations need redoing.

--- Descent 64 v1/Assets/Scripts/Cutscenes/Cutscene 3 Manager.cs
+++ Descent 64 v1/Assets/Scripts/Cutscenes/Cutscene 3 Manager.cs
@@ -118,6 +118,10 @@ public class Cutscene3Manager : MonoBehaviour
                 // change color and speed
                 dialogueText.color = Color.red;
             }
+            if (shotNumber == 11){
+                magicBall.SetActive(false);
+                dialogueText.color = Color.white;
+            }
             
 
             
