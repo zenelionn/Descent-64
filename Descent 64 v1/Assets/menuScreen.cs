f25fe4aefbf783dda91165524b66a1837416ea15
Eli <148476551+zenelionn@users.noreply.github.com>
2025-03-18 15:01:46 +0000

    remade menu to have a new animation and new looking buttons

--- Descent 64 v1/Assets/menuScreen.cs
+++ Descent 64 v1/Assets/menuScreen.cs
@@ -8,9 +8,11 @@ using UnityEngine.SceneManagement;
 public class menuScreen : MonoBehaviour
 {
     [SerializeField] private GameObject contentWarningScreen;
+    [SerializeField] private GameObject settingsScreen;
 
     private void Start(){
         contentWarningScreen.SetActive(false);
+        settingsScreen.SetActive(false);
     }
     public void PlayGame(){
         SceneManager.LoadScene("Basement Cutscenes");
@@ -20,7 +22,12 @@ public class menuScreen : MonoBehaviour
         contentWarningScreen.SetActive(true);
     }
 
+    public void Settings(){
+        settingsScreen.SetActive(true);
+    }
+
     public void ReturnToMain(){
         contentWarningScreen.SetActive(false);
+        settingsScreen.SetActive(false);
     }
 }
