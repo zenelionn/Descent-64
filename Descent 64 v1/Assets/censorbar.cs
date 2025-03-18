1a170620f10516622a2c38fbcdfe5f071aedbbba
Eli <148476551+zenelionn@users.noreply.github.com>
2025-03-18 13:26:05 +0000

    added flavour text for when you approach David's censor bar

--- Descent 64 v1/Assets/censorbar.cs
+++ Descent 64 v1/Assets/censorbar.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class censorbar : MonoBehaviour
 {
@@ -8,7 +10,14 @@ public class censorbar : MonoBehaviour
     // set enemy pos to cutscene 4 death pos
 
     // have the censor bar always be looking at the player to hide the body
+    [Header("Canvas Stuff")]
+    [SerializeField] private Canvas dialogueCanvas;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField] private GameObject EButton;
 
+    [Header("Interacting with the Censor")]
+    private bool canInteract;
+    [SerializeField] private KeyCode interactionKey = KeyCode.E;
 
     [SerializeField] private GameObject enemy;
     [SerializeField] private Animator enemyAnimator;
@@ -18,6 +27,10 @@ public class censorbar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        dialogueCanvas.enabled = false;
+        canInteract = false;
+        EButton.GetComponent<Image>().color = new Color32(70,70,70,255);
+
         //enemy.transform.position = enemyHealth.enemyDeathPos;
         if (Cutscene4Manager.DavidDead == false){
             censorBar.SetActive(false);
@@ -33,9 +46,27 @@ public class censorbar : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter(Collider other){
+        EButton.GetComponent<Image>().color = new Color32(255,255,255,255);
+        canInteract = true;
+
+    }
+
+    private void OnTriggerExit(Collider other){
+        EButton.GetComponent<Image>().color = new Color32(70,70,70,255);
+        canInteract = false;
+        dialogueCanvas.enabled = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
         censorBar.transform.LookAt(playerCam.transform.position);
+
+        if (canInteract && Input.GetKeyDown(interactionKey)){
+            dialogueText.SetText("I can't look at him...");
+            dialogueCanvas.enabled = true;
+        }
+        
     }
 }
